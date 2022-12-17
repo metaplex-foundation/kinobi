1c438c797e024a5814855cf31c1e618bd937d99a
Loris Leiva <loris.leiva@gmail.com>
2022-12-17 16:51:10 +0000

    wip

--- src/nodes/AccountNode.ts
+++ src/nodes/AccountNode.ts
@@ -1,17 +1,22 @@
 import type { IdlAccount } from 'src/idl';
 import type { Visitable, Visitor } from '../visitors';
+import { createTypeNodeFromIdl } from './TypeNode';
+import { TypeStructNode } from './TypeStructNode';
 
 export class AccountNode implements Visitable {
   constructor(
     readonly name: string,
-    // readonly type: TypeStructNode,
+    readonly type: TypeStructNode,
     readonly docs: string[] = [],
   ) {}
 
   static fromIdl(idl: Partial<IdlAccount>): AccountNode {
     const name = idl.name ?? '';
+    const type = createTypeNodeFromIdl(
+      idl.type ?? { kind: 'struct', fields: [] },
+    ) as TypeStructNode;
     const docs = idl.docs ?? [];
-    return new AccountNode(name, docs);
+    return new AccountNode(name, type, docs);
   }
 
   visit(visitor: Visitor): void {
--- src/nodes/TypeNode.ts
+++ src/nodes/TypeNode.ts
@@ -1,12 +1,31 @@
 import type { IdlType } from 'src/idl';
 import { TypeLeafNode } from './TypeLeafNode';
+import { TypeStructNode } from './TypeStructNode';
 
-export type TypeNode = TypeLeafNode;
+export type TypeNode = TypeLeafNode | TypeStructNode;
 
 export const createTypeNodeFromIdl = (idlType: IdlType): TypeNode => {
+  // Handle leaf types such as 'u8', 'u64', 'publicKey', etc.
   if (typeof idlType === 'string') {
     return new TypeLeafNode(idlType);
   }
 
+  // Ensure eveything else is an object.
+  if (typeof idlType !== 'object') {
+    throw new Error(`TypeNode: Unsupported type ${JSON.stringify(idlType)}`);
+  }
+
+  // Handle struct types with fields.
+  if ('kind' in idlType && idlType.kind === 'struct') {
+    const fields = idlType.fields.map((field) => ({
+      name: field.name ?? '',
+      type: createTypeNodeFromIdl(field.type),
+      docs: field.docs ?? [],
+    }));
+
+    return new TypeStructNode(fields);
+  }
+
+  // Throw an error for unsupported types.
   throw new Error(`TypeNode: Unsupported type ${JSON.stringify(idlType)}`);
 };
