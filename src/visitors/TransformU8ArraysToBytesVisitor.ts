b880619b2b5a58d3f95d8d331b4e7528a6ca2f48
Loris Leiva <loris.leiva@gmail.com>
2022-12-18 09:17:01 +0000

    wip

--- src/visitors/TransformU8ArraysToBytesVisitor.ts
+++ src/visitors/TransformU8ArraysToBytesVisitor.ts
@@ -2,6 +2,10 @@ import * as nodes from '../nodes';
 import { BaseNodeVisitor } from './BaseNodeVisitor';
 
 export class TransformU8ArraysToBytesVisitor extends BaseNodeVisitor {
+  constructor(readonly sizes: number[] | '*' = [32, 64]) {
+    super();
+  }
+
   visitTypeArray(typeArray: nodes.TypeArrayNode): nodes.Node {
     const type = typeArray.itemType.accept(this);
     nodes.assertTypeNode(type);
@@ -9,11 +13,15 @@ export class TransformU8ArraysToBytesVisitor extends BaseNodeVisitor {
     if (
       nodes.isTypeLeafNode(type) &&
       type.type === 'u8' &&
-      typeArray.size === 32
+      this.hasRequiredSize(typeArray.size)
     ) {
       return new nodes.TypeLeafNode('bytes');
     }
 
     return new nodes.TypeArrayNode(type, typeArray.size);
+  }
+
+  protected hasRequiredSize(size: number): boolean {
+    return this.sizes === '*' || this.sizes.includes(size);
   }
 }
