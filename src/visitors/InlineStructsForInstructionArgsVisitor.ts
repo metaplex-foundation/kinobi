9b2362435f929b61c670025ffb9a5a9edd6101f6
Loris Leiva <loris.leiva@gmail.com>
2022-12-21 14:36:26 +0000

    wip

--- src/visitors/InlineStructsForInstructionArgsVisitor.ts
+++ src/visitors/InlineStructsForInstructionArgsVisitor.ts
@@ -2,15 +2,9 @@ import * as nodes from '../nodes';
 import { BaseNodeVisitor } from './BaseNodeVisitor';
 
 export class InlineStructsForInstructionArgsVisitor extends BaseNodeVisitor {
-  visitAccount(account: nodes.AccountNode): nodes.Node {
-    return account;
-  }
-
   visitInstruction(instruction: nodes.InstructionNode): nodes.Node {
-    const visitedArgs = instruction.args.accept(this);
-    nodes.assertTypeStructNode(visitedArgs);
-
-    const inlinedArgs = visitedArgs.fields.reduce<nodes.TypeStructNodeField[]>(
+    const currentArgs = instruction.args;
+    const inlinedArgs = currentArgs.fields.reduce<nodes.TypeStructNodeField[]>(
       (all, one) => {
         if (nodes.isTypeStructNode(one.type)) {
           all.push(...one.type.fields);
@@ -31,14 +25,20 @@ export class InlineStructsForInstructionArgsVisitor extends BaseNodeVisitor {
       instruction.program,
       instruction.accounts,
       hasConflictingNames
-        ? visitedArgs
-        : new nodes.TypeStructNode(visitedArgs.name, inlinedArgs),
+        ? currentArgs
+        : new nodes.TypeStructNode(currentArgs.name, inlinedArgs),
       instruction.discriminator,
       instruction.defaultOptionalAccounts,
     );
   }
 
+  visitAccount(account: nodes.AccountNode): nodes.Node {
+    // No need to visit the account trees.
+    return account;
+  }
+
   visitDefinedType(definedType: nodes.DefinedTypeNode): nodes.Node {
+    // No need to visit the defined type trees.
     return definedType;
   }
 }
