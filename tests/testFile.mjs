f11228128044bc8d2d27ca7781ac594fd064ae45
Loris Leiva <loris.leiva@gmail.com>
2022-12-22 22:05:25 +0000

    wip

--- tests/testFile.mjs
+++ tests/testFile.mjs
@@ -1,19 +1,22 @@
 import {
-  RenderJavaScriptVisitor,
-  Solita,
+  IdentifyDefaultInstructionAccountsVisitor,
   InlineDefinedTypesForInstructionArgsVisitor,
   InlineDefinedTypesVisitor,
   InlineStructsForInstructionArgsVisitor,
   PrintVisitor,
+  RenderJavaScriptVisitor,
+  Solita,
   TransformU8ArraysToBytesVisitor,
 } from '../dist/index.js';
 
 const solita = new Solita('./tests/idl.json');
+console.log('\n', '--- BEFORE VISITORS ---', '\n');
 solita.accept(new PrintVisitor());
 console.log('\n', '--- AFTER VISITORS ---', '\n');
+solita.update(new IdentifyDefaultInstructionAccountsVisitor());
 solita.update(new TransformU8ArraysToBytesVisitor());
 solita.update(new InlineDefinedTypesVisitor(['Payload', 'SeedsVec']));
 solita.update(new InlineDefinedTypesForInstructionArgsVisitor());
 solita.update(new InlineStructsForInstructionArgsVisitor());
 solita.accept(new PrintVisitor());
-solita.accept(new RenderJavaScriptVisitor('./generated'));
+solita.accept(new RenderJavaScriptVisitor('./package/src/generated'));
