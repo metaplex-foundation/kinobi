e8de37393d978191871f872f455b92693d156db3
Loris Leiva <loris.leiva@gmail.com>
2022-12-26 12:22:52 +0000

    wip

--- tests/testFile.cjs
+++ tests/testFile.cjs
@@ -2,20 +2,18 @@ const {
   RenderJavaScriptVisitor,
   Kinobi,
   InlineDefinedTypesForInstructionArgsVisitor,
-  InlineDefinedTypesVisitor,
   InlineStructsForInstructionArgsVisitor,
   PrintVisitor,
   TransformU8ArraysToBytesVisitor,
   IdentifyDefaultInstructionAccountsVisitor,
 } = require('../dist/index.js');
 
-const kinobi = new Kinobi(__dirname + '/mpl_token_metadata.json');
+const kinobi = new Kinobi(__dirname + '/mpl_token_metadata.json', false);
 console.log('\n', '--- BEFORE VISITORS ---', '\n');
 kinobi.accept(new PrintVisitor());
 console.log('\n', '--- AFTER VISITORS ---', '\n');
 kinobi.update(new IdentifyDefaultInstructionAccountsVisitor());
 kinobi.update(new TransformU8ArraysToBytesVisitor());
-kinobi.update(new InlineDefinedTypesVisitor(['Payload', 'SeedsVec']));
 kinobi.update(new InlineDefinedTypesForInstructionArgsVisitor());
 kinobi.update(new InlineStructsForInstructionArgsVisitor());
 kinobi.accept(new PrintVisitor());
