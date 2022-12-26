11ed01d5175787e518133d277f31fdd8671690f6
Loris Leiva <loris.leiva@gmail.com>
2022-12-26 12:30:38 +0000

    wip

--- src/visitors/InlineDefinedTypesForInstructionArgsVisitor.ts
+++ src/visitors/InlineDefinedTypesForInstructionArgsVisitor.ts
@@ -6,12 +6,25 @@ import { InlineDefinedTypesVisitor } from './InlineDefinedTypesVisitor';
 
 export class InlineDefinedTypesForInstructionArgsVisitor extends BaseRootVisitor {
   visitRoot(root: nodes.RootNode): nodes.RootNode {
+    // Get all defined types used exactly once as an instruction argument.
     const histogram = root.accept(new GetDefinedTypeHistogramVisitor());
-    const definedTypesToInline = Object.keys(histogram).filter(
+    let definedTypesToInline: string[] = Object.keys(histogram).filter(
       (key) =>
         (histogram[key].total ?? 0) === 1 &&
         (histogram[key].directlyAsInstructionArgs ?? 0) === 1
     );
+
+    // Filter out scalar enums which need to be defined as an external type.
+    const { allDefinedTypes } = root;
+    definedTypesToInline = definedTypesToInline.filter((definedtype) => {
+      const found = allDefinedTypes.find(({ name }) => name === definedtype);
+      if (!found) return false;
+      const isScalarEnum =
+        nodes.isTypeEnumNode(found.type) && found.type.isScalarEnum();
+      return !isScalarEnum;
+    });
+
+    // Inline the identified defined types.
     const inlineVisitor = new InlineDefinedTypesVisitor(definedTypesToInline);
     const newRoot = root.accept(inlineVisitor);
     assertRootNode(newRoot);
