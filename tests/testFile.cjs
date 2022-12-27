f9e11077ae665d7114a272c469538140fc5d375b
Loris Leiva <loris.leiva@gmail.com>
2022-12-27 10:07:33 +0000

    wip

--- tests/testFile.cjs
+++ tests/testFile.cjs
@@ -2,8 +2,19 @@ const {
   RenderJavaScriptVisitor,
   Kinobi,
   PrintVisitor,
+  RenameNodesVisitor,
 } = require('../dist/index.js');
 
 const kinobi = new Kinobi(__dirname + '/mpl_token_metadata.json');
+kinobi.update(
+  new RenameNodesVisitor({
+    mplTokenMetadata: {
+      instructions: {
+        Create: 'CreateDigitalAsset',
+        Update: 'UpdateDigitalAsset',
+      },
+    },
+  })
+);
 kinobi.accept(new PrintVisitor());
 kinobi.accept(new RenderJavaScriptVisitor('./package/src/generated'));
