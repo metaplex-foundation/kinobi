5bea4ab0dfbf96bbf6cb561b630de8073f067b2f
Loris Leiva <loris.leiva@gmail.com>
2023-01-10 12:03:16 +0000

    wip

--- src/visitors/transformers/SetLeafWrappersVisitor.ts
+++ src/visitors/transformers/SetLeafWrappersVisitor.ts
@@ -6,10 +6,8 @@ type LeafWrapperMap = Record<string, nodes.LeafWrapper>;
 export class SetLeafWrappersVisitor extends TransformNodesVisitor {
   constructor(readonly map: LeafWrapperMap) {
     const transforms = Object.entries(map).map(
-      ([selector, wrapper]): NodeTransform => ({
-        selector: (node, stack) =>
-          nodes.isTypeLeafNode(node) &&
-          stack.matchesWithNames(selector.split('.')),
+      ([selectorStack, wrapper]): NodeTransform => ({
+        selector: { type: 'typeLeaf', stack: selectorStack },
         transformer: (node) => {
           nodes.assertTypeLeafNode(node);
           return new nodes.TypeLeafNode(node.type, wrapper);
