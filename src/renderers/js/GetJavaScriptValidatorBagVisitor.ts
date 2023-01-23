c9e30ffa7d830029e88a7adb7f842b735d47fd28
Loris Leiva <loris.leiva@gmail.com>
2023-01-23 16:35:28 +0000

    wip

--- src/renderers/js/GetJavaScriptValidatorBagVisitor.ts
+++ src/renderers/js/GetJavaScriptValidatorBagVisitor.ts
@@ -183,7 +183,8 @@ export class GetJavaScriptValidatorBagVisitor extends GetDefaultValidatorBagVisi
       const message =
         `The ${this.getNodeTitle(node)} ${exportDetails}` +
         `conflicts with the ${conflictExportDetails}` +
-        `${this.getNodeTitle(conflictingNode)}.`;
+        `${this.getNodeTitle(conflictingNode)}. ` +
+        `(Conflicting stack: ${exportConflict.stack})).`;
       bag.error(message, node, stack);
     });
     return bag;
