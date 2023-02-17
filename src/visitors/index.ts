7801089f14f60654adaeb978de5458556f8e1c49
Loris Leiva <loris.leiva@gmail.com>
2023-02-17 11:34:53 +0000

    Improve typedoc settings

--- src/visitors/index.ts
+++ src/visitors/index.ts
@@ -3,6 +3,7 @@ export * from './transformers';
 export * from './workers';
 
 export * from './BaseDelegateVisitor';
+export * from './BaseNodeOrNullVisitor';
 export * from './BaseNodeVisitor';
 export * from './BaseThrowVisitor';
 export * from './BaseVoidVisitor';
