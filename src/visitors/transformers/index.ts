d35129da533f40197bcca4085e20794307fbffe7
Loris Leiva <loris.leiva@gmail.com>
2023-03-03 15:10:19 +0000

    Create use custom serializer visitors

--- src/visitors/transformers/index.ts
+++ src/visitors/transformers/index.ts
@@ -21,3 +21,5 @@ export * from './UpdateDefinedTypesVisitor';
 export * from './UpdateErrorsVisitor';
 export * from './UpdateInstructionsVisitor';
 export * from './UpdateProgramsVisitor';
+export * from './UseCustomAccountSerializerVisitor';
+export * from './UseCustomInstructionSerializerVisitor';
