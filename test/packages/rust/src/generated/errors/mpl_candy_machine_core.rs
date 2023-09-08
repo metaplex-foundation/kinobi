7dbf11bb0494abf0318125001d1b643a318c0e74
febo <febo@metaplex.com>
2023-09-08 21:09:03 +0100

    Add decimal indices to error comments

--- test/packages/rust/src/generated/errors/mpl_candy_machine_core.rs
+++ test/packages/rust/src/generated/errors/mpl_candy_machine_core.rs
@@ -10,67 +10,67 @@ use thiserror::Error;
 
 #[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
 pub enum MplCandyMachineCoreError {
-    /// 0x1770 - Account does not have correct owner
+    /// 6000 (0x1770) - Account does not have correct owner
     #[error("Account does not have correct owner")]
     IncorrectOwner,
-    /// 0x1771 - Account is not initialized
+    /// 6001 (0x1771) - Account is not initialized
     #[error("Account is not initialized")]
     Uninitialized,
-    /// 0x1772 - Mint Mismatch
+    /// 6002 (0x1772) - Mint Mismatch
     #[error("Mint Mismatch")]
     MintMismatch,
-    /// 0x1773 - Index greater than length
+    /// 6003 (0x1773) - Index greater than length
     #[error("Index greater than length")]
     IndexGreaterThanLength,
-    /// 0x1774 - Numerical overflow error
+    /// 6004 (0x1774) - Numerical overflow error
     #[error("Numerical overflow error")]
     NumericalOverflowError,
-    /// 0x1775 - Can only provide up to 4 creators to candy machine (because candy machine is one)
+    /// 6005 (0x1775) - Can only provide up to 4 creators to candy machine (because candy machine is one)
     #[error("Can only provide up to 4 creators to candy machine (because candy machine is one)")]
     TooManyCreators,
-    /// 0x1776 - Candy machine is empty
+    /// 6006 (0x1776) - Candy machine is empty
     #[error("Candy machine is empty")]
     CandyMachineEmpty,
-    /// 0x1777 - Candy machines using hidden uris do not have config lines, they have a single hash representing hashed order
+    /// 6007 (0x1777) - Candy machines using hidden uris do not have config lines, they have a single hash representing hashed order
     #[error("Candy machines using hidden uris do not have config lines, they have a single hash representing hashed order")]
     HiddenSettingsDoNotHaveConfigLines,
-    /// 0x1778 - Cannot change number of lines unless is a hidden config
+    /// 6008 (0x1778) - Cannot change number of lines unless is a hidden config
     #[error("Cannot change number of lines unless is a hidden config")]
     CannotChangeNumberOfLines,
-    /// 0x1779 - Cannot switch to hidden settings after items available is greater than 0
+    /// 6009 (0x1779) - Cannot switch to hidden settings after items available is greater than 0
     #[error("Cannot switch to hidden settings after items available is greater than 0")]
     CannotSwitchToHiddenSettings,
-    /// 0x177A - Incorrect collection NFT authority
+    /// 6010 (0x177A) - Incorrect collection NFT authority
     #[error("Incorrect collection NFT authority")]
     IncorrectCollectionAuthority,
-    /// 0x177B - The metadata account has data in it, and this must be empty to mint a new NFT
+    /// 6011 (0x177B) - The metadata account has data in it, and this must be empty to mint a new NFT
     #[error("The metadata account has data in it, and this must be empty to mint a new NFT")]
     MetadataAccountMustBeEmpty,
-    /// 0x177C - Can't change collection settings after items have begun to be minted
+    /// 6012 (0x177C) - Can't change collection settings after items have begun to be minted
     #[error("Can't change collection settings after items have begun to be minted")]
     NoChangingCollectionDuringMint,
-    /// 0x177D - Value longer than expected maximum value
+    /// 6013 (0x177D) - Value longer than expected maximum value
     #[error("Value longer than expected maximum value")]
     ExceededLengthError,
-    /// 0x177E - Missing config lines settings
+    /// 6014 (0x177E) - Missing config lines settings
     #[error("Missing config lines settings")]
     MissingConfigLinesSettings,
-    /// 0x177F - Cannot increase the length in config lines settings
+    /// 6015 (0x177F) - Cannot increase the length in config lines settings
     #[error("Cannot increase the length in config lines settings")]
     CannotIncreaseLength,
-    /// 0x1780 - Cannot switch from hidden settings
+    /// 6016 (0x1780) - Cannot switch from hidden settings
     #[error("Cannot switch from hidden settings")]
     CannotSwitchFromHiddenSettings,
-    /// 0x1781 - Cannot change sequential index generation after items have begun to be minted
+    /// 6017 (0x1781) - Cannot change sequential index generation after items have begun to be minted
     #[error("Cannot change sequential index generation after items have begun to be minted")]
     CannotChangeSequentialIndexGeneration,
-    /// 0x1782 - Collection public key mismatch
+    /// 6018 (0x1782) - Collection public key mismatch
     #[error("Collection public key mismatch")]
     CollectionKeyMismatch,
-    /// 0x1783 - Could not retrive config line data
+    /// 6019 (0x1783) - Could not retrive config line data
     #[error("Could not retrive config line data")]
     CouldNotRetrieveConfigLineData,
-    /// 0x1784 - Not all config lines were added to the candy machine
+    /// 6020 (0x1784) - Not all config lines were added to the candy machine
     #[error("Not all config lines were added to the candy machine")]
     NotFullyLoaded,
 }
