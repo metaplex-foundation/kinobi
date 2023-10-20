2506ae409e74d4ab7c187bc01e75a5b5498163c1
Loris Leiva <loris.leiva@gmail.com>
2023-10-20 09:43:43 +0100

    wip

---
+++ test/packages/js-experimental/src/hooked/resolvers.ts
@@ -0,0 +1,35 @@
+import { Base58EncodedAddress, ProgramDerivedAddress } from '@solana/addresses';
+import {
+  Context,
+  ResolvedAccount,
+  TokenStandard,
+  expectAddress,
+  findMasterEditionV2Pda,
+} from '../generated';
+
+export const resolveMasterEditionFromTokenStandard = async (
+  context: Pick<Context, 'getProgramAddress' | 'getProgramDerivedAddress'>,
+  accounts: Record<string, ResolvedAccount>,
+  args: { tokenStandard?: TokenStandard },
+  _programId: any,
+  _isWritable: any
+): Promise<Partial<{ value: ProgramDerivedAddress | null }>> => {
+  return args.tokenStandard === TokenStandard.NonFungible ||
+    args.tokenStandard === TokenStandard.ProgrammableNonFungible
+    ? {
+        value: await findMasterEditionV2Pda(context, {
+          mint: expectAddress(accounts.mint?.value),
+        }),
+      }
+    : { value: null };
+};
+
+export const resolveTokenOrAta = async (
+  _context: any,
+  _accounts: Record<string, ResolvedAccount>,
+  args: { proof?: Base58EncodedAddress[] },
+  _programId?: any,
+  _isWritable?: any
+): Promise<boolean> => {
+  return !!args.proof && args.proof.length > 0;
+};
