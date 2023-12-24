59965741f8c29435e5a55f5dd790875d6e30b591
Loris Leiva <loris.leiva@gmail.com>
2023-12-24 10:30:21 +0000

    wip

--- src/visitors/Visitor.ts
+++ src/visitors/Visitor.ts
@@ -1,24 +1,28 @@
 import { KinobiError, pascalCase } from '../shared';
 import * as nodes from '../nodes';
 
-export type Visitor<T = void> = {
-  [K in keyof nodes.RegisteredNodes as K extends `${infer KWithoutNode}Node`
-    ? `visit${Capitalize<KWithoutNode>}`
-    : `visit${Capitalize<K>}`]: (node: nodes.RegisteredNodes[K]) => T;
+export type Visitor<
+  TReturn = void,
+  TNodeKeys extends keyof nodes.RegisteredNodes = keyof nodes.RegisteredNodes
+> = {
+  [K in TNodeKeys as GetVisitorFunctionName<K>]: (
+    node: nodes.RegisteredNodes[K]
+  ) => TReturn;
 };
+
+export type GetVisitorFunctionName<T extends nodes.Node['kind']> =
+  T extends `${infer TWithoutNode}Node`
+    ? `visit${Capitalize<TWithoutNode>}`
+    : never;
 
 export function visit<T>(node: nodes.Node, visitor: Visitor<T>): T {
   return visitor[getVisitFunctionName(node.kind)](node as any);
 }
 
-export function getVisitFunctionName(node: nodes.Node['kind']) {
+export function getVisitFunctionName<T extends nodes.Node['kind']>(node: T) {
   if (!nodes.REGISTERED_NODES_KEYS.includes(node)) {
     throw new KinobiError(`Unrecognized node [${node}]`);
   }
 
-  return `visit${pascalCase(
-    node.slice(0, -4)
-  )}` as typeof node extends `${infer KWithoutNode}Node`
-    ? `visit${Capitalize<KWithoutNode>}`
-    : `visit${Capitalize<typeof node>}`;
+  return `visit${pascalCase(node.slice(0, -4))}` as GetVisitorFunctionName<T>;
 }
