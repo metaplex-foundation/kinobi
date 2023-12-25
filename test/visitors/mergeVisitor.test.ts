9edb8f4ad016f30cdc5bc6fe9ef02b492f725664
Loris Leiva <loris.leiva@gmail.com>
2023-12-25 17:33:38 +0000

    wip

--- test/visitors/mergeVisitor.test.ts
+++ test/visitors/mergeVisitor.test.ts
@@ -7,19 +7,19 @@ import {
   visit,
 } from '../../src';
 
-// This visitor sets the node kind for all leaves and combines
-// them together such that each node lists the kind of its children.
-const mergeKindVisitor = mergeVisitor(
-  (node) => node.kind as string,
-  (node, values) => `${node.kind}(${values.join(',')})`
-);
-
 test('it sets a value for all leaves and merges node values together', async (t) => {
   // Given the following 3-nodes tree.
   const node = tupleTypeNode([numberTypeNode('u32'), publicKeyTypeNode()]);
 
-  // When we visit the tree with the mergeKindVisitor.
-  const result = visit(node, mergeKindVisitor);
+  // And a visitor that sets the node kind for all leaves and combines
+  // them together such that each node lists the kind of its children.
+  const visitor = mergeVisitor(
+    (node) => node.kind as string,
+    (node, values) => `${node.kind}(${values.join(',')})`
+  );
+
+  // When we visit the tree using that visitor.
+  const result = visit(node, visitor);
 
   // Then we get the following result.
   t.is(result, 'tupleTypeNode(numberTypeNode,publicKeyTypeNode)');
@@ -38,4 +38,26 @@ test('it can be used to count nodes', async (t) => {
 
   // Then we expect to have 3 nodes.
   t.is(result, 3);
+});
+
+test('it can create partial visitors', async (t) => {
+  // Given the following 3-nodes tree.
+  const node = tupleTypeNode([numberTypeNode('u32'), publicKeyTypeNode()]);
+
+  // And a visitor that only supports 2 of these nodes.
+  const visitor = mergeVisitor(
+    (node) => node.kind as string,
+    (node, values) => `${node.kind}(${values.join(',')})`,
+    { nodeKeys: ['tupleTypeNode', 'numberTypeNode'] }
+  );
+
+  // When we visit the tree using that visitor.
+  const result = visit(node, visitor);
+
+  // Then the unsupported node is not included in the result.
+  t.is(result, 'tupleTypeNode(numberTypeNode)');
+
+  // And the unsupported node cannot be visited.
+  // @ts-expect-error
+  t.throws(() => visit(publicKeyTypeNode(), visitor));
 });
