cfce9c477b2c9c070366638639f0819188841b15
Loris Leiva <loris.leiva@gmail.com>
2023-12-26 13:34:41 +0000

    wip

--- src/visitors/topDownTransformerVisitor.ts
+++ src/visitors/topDownTransformerVisitor.ts
@@ -1,5 +1,5 @@
 import * as nodes from '../nodes';
-import { NodeStack } from '../shared';
+import { NodeSelector, NodeStack, getNodeSelectorFunction } from '../shared';
 import { Visitor } from './Visitor';
 import { IdentityVisitorInterceptor, identityVisitor } from './identityVisitor';
 
@@ -8,23 +8,41 @@ export type TopDownNodeTransformer<TNode extends nodes.Node = nodes.Node> = <
 >(
   node: T,
   stack: NodeStack
-) => T;
+) => T | null;
+
+export type TopDownNodeTransformerWithSelector<
+  TNode extends nodes.Node = nodes.Node
+> = {
+  select: NodeSelector;
+  transform: TopDownNodeTransformer<TNode>;
+};
 
 export function topDownTransformerVisitor<
   TNodeKeys extends keyof nodes.RegisteredNodes = keyof nodes.RegisteredNodes
 >(
-  transformers: TopDownNodeTransformer[],
+  transformers: (TopDownNodeTransformer | TopDownNodeTransformerWithSelector)[],
   options: {
     nodeKeys?: TNodeKeys[];
   } = {}
 ): Visitor<nodes.Node | null, TNodeKeys> {
+  const transformerFunctions = transformers.map(
+    (transformer): TopDownNodeTransformer =>
+      typeof transformer === 'function'
+        ? transformer
+        : (node, stack) =>
+            getNodeSelectorFunction(transformer.select)(node, stack)
+              ? transformer.transform(node, stack)
+              : node
+  );
+
   const stack = new NodeStack();
   const intercept: IdentityVisitorInterceptor = (fn) => (node) => {
-    const appliedNode = transformers.reduce(
-      (acc, transformer) => transformer(acc, stack.clone()),
-      node
+    const appliedNode = transformerFunctions.reduce(
+      (acc, transformer) =>
+        acc === null ? null : transformer(acc, stack.clone()),
+      node as Parameters<typeof fn>[0] | null
     );
-
+    if (appliedNode === null) return null;
     stack.push(appliedNode);
     const newNode = fn(appliedNode);
     stack.pop();
