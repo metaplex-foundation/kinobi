bd2bce857b9ba22b88ea96bb55f4d1e9caf3e2bb
Loris Leiva <loris.leiva@gmail.com>
2023-12-27 18:06:59 +0000

    wip

--- src/visitors/visitor.ts
+++ src/visitors/visitor.ts
@@ -1,30 +1,27 @@
+import { Node, REGISTERED_NODES_KEYS, RegisteredNodes } from '../nodes';
 import { KinobiError, pascalCase } from '../shared';
-import * as nodes from '../nodes';
 
 export type Visitor<
   TReturn = void,
-  TNodeKeys extends keyof nodes.RegisteredNodes = keyof nodes.RegisteredNodes
+  TNodeKeys extends keyof RegisteredNodes = keyof RegisteredNodes
 > = {
   [K in TNodeKeys as GetVisitorFunctionName<K>]: (
-    node: nodes.RegisteredNodes[K]
+    node: RegisteredNodes[K]
   ) => TReturn;
 };
 
-export type GetVisitorFunctionName<T extends nodes.Node['kind']> =
+export type GetVisitorFunctionName<T extends Node['kind']> =
   T extends `${infer TWithoutNode}Node`
     ? `visit${Capitalize<TWithoutNode>}`
     : never;
 
 // This first overload enables class-based visitors.
-export function visit<TReturn>(
-  node: nodes.Node,
-  visitor: Visitor<TReturn>
-): TReturn;
-export function visit<TReturn, TNode extends nodes.Node>(
+export function visit<TReturn>(node: Node, visitor: Visitor<TReturn>): TReturn;
+export function visit<TReturn, TNode extends Node>(
   node: TNode,
   visitor: Visitor<TReturn, TNode['kind']>
 ): TReturn;
-export function visit<TReturn, TNode extends nodes.Node>(
+export function visit<TReturn, TNode extends Node>(
   node: TNode,
   visitor: Visitor<TReturn, TNode['kind']>
 ): TReturn {
@@ -34,8 +31,8 @@ export function visit<TReturn, TNode extends nodes.Node>(
   return (visitor[key] as typeof visitor[typeof key] & Function)(node);
 }
 
-export function getVisitFunctionName<T extends nodes.Node['kind']>(node: T) {
-  if (!nodes.REGISTERED_NODES_KEYS.includes(node)) {
+export function getVisitFunctionName<T extends Node['kind']>(node: T) {
+  if (!REGISTERED_NODES_KEYS.includes(node)) {
     throw new KinobiError(`Unrecognized node [${node}]`);
   }
 
