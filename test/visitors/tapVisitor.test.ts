e47f5448f3d1d7db452f0084d20773ccfe309797
Loris Leiva <loris.leiva@gmail.com>
2023-12-27 18:25:43 +0000

    wip

---
+++ test/visitors/tapVisitor.test.ts
@@ -0,0 +1,39 @@
+import test from 'ava';
+import {
+  mergeVisitor,
+  numberTypeNode,
+  publicKeyTypeNode,
+  tapVisitor,
+  tupleTypeNode,
+  visit,
+} from '../../src';
+
+test.skip('it returns the same visitor whilst tapping into one of its visits', (t) => {
+  // Given the following tree.
+  const node = tupleTypeNode([
+    numberTypeNode('u32'),
+    tupleTypeNode([numberTypeNode('u32'), publicKeyTypeNode()]),
+  ]);
+
+  // And a sum visitor that counts the nodes.
+  const visitor = mergeVisitor(
+    () => 1,
+    (_, values) => values.reduce((a, b) => a + b, 1)
+  );
+
+  // And a tap visitor that taps into the numberTypeNode visit and counts them.
+  let numberOfNumberNodes = 0;
+  const tappedVisitor = tapVisitor(visitor, 'numberTypeNode', () => {
+    console.log('here');
+    numberOfNumberNodes++;
+  });
+
+  // When we visit the tree using the tapped visitor.
+  const result = visit(node, tappedVisitor);
+
+  // Then we get the expected result.
+  t.is(result, 5);
+
+  // And the tapped counter is also correct.
+  t.is(numberOfNumberNodes, 2);
+});
