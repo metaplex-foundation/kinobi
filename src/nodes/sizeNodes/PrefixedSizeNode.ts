d550a8527d04c737b7180557906fcb710b75f176
Loris Leiva <loris.leiva@gmail.com>
2023-12-29 17:05:22 +0000

    Fix typo

--- src/nodes/sizeNodes/PrefixedSizeNode.ts
+++ src/nodes/sizeNodes/PrefixedSizeNode.ts
@@ -10,16 +10,16 @@ export function prefixedSizeNode(prefix: NumberTypeNode): PrefixedSizeNode {
   return { kind: 'prefixedSizeNode', prefix };
 }
 
-export function isRemainderSizeNode(
+export function isPrefixedSizeNode(
   node: Node | null
 ): node is PrefixedSizeNode {
   return !!node && node.kind === 'prefixedSizeNode';
 }
 
-export function assertRemainderSizeNode(
+export function assertPrefixedSizeNode(
   node: Node | null
 ): asserts node is PrefixedSizeNode {
-  if (!isRemainderSizeNode(node)) {
+  if (!isPrefixedSizeNode(node)) {
     throw new Error(`Expected prefixedSizeNode, got ${node?.kind ?? 'null'}.`);
   }
 }
