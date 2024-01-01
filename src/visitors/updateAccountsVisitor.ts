9ea62549d2ea0c615e07d647b269d5d268a5c86e
Loris Leiva <loris.leiva@gmail.com>
2024-01-01 13:59:39 +0000

    Update updateAccountsVisitor.ts

--- src/visitors/updateAccountsVisitor.ts
+++ src/visitors/updateAccountsVisitor.ts
@@ -1,10 +1,16 @@
 import {
   AccountNodeInput,
+  PdaNode,
+  PdaSeedNode,
   accountDataNode,
+  accountLinkNode,
   accountNode,
   assertIsNode,
+  pdaLinkNode,
+  pdaNode,
+  programNode,
 } from '../nodes';
-import { mainCase, renameStructNode } from '../shared';
+import { MainCaseString, mainCase, renameStructNode } from '../shared';
 import {
   BottomUpNodeTransformerWithSelector,
   bottomUpTransformerVisitor,
@@ -14,35 +20,120 @@ export type AccountUpdates =
   | { delete: true }
   | (Partial<Omit<AccountNodeInput, 'data'>> & {
       data?: Record<string, string>;
+      seeds?: PdaSeedNode[];
     });
 
 export function updateAccountsVisitor(map: Record<string, AccountUpdates>) {
   return bottomUpTransformerVisitor(
-    Object.entries(map).map(
-      ([selector, updates]): BottomUpNodeTransformerWithSelector => {
-        const selectorStack = selector.split('.');
-        const name = selectorStack.pop();
-        return {
+    Object.entries(map).flatMap(([selector, updates]) => {
+      const selectorStack = selector.split('.');
+      const name = selectorStack.pop();
+      const newName =
+        typeof updates === 'object' && 'name' in updates && updates.name
+          ? mainCase(updates.name)
+          : undefined;
+      const pdasToUpsert = [] as { program: MainCaseString; pda: PdaNode }[];
+
+      const transformers: BottomUpNodeTransformerWithSelector[] = [
+        {
           select: `${selectorStack.join('.')}.[accountNode]${name}`,
-          transform: (node) => {
+          transform: (node, stack) => {
             assertIsNode(node, 'accountNode');
-            if ('delete' in updates) {
-              return null;
+            if ('delete' in updates) return null;
+
+            const { seeds, pda, ...assignableUpdates } = updates;
+            let newPda = node.pda;
+            if (pda && !pda.importFrom && seeds) {
+              newPda = pda;
+              pdasToUpsert.push({
+                program: stack.getProgram()!.name,
+                pda: pdaNode(pda.name, seeds),
+              });
+            } else if (pda) {
+              newPda = pda;
+            } else if (seeds && node.pda) {
+              pdasToUpsert.push({
+                program: stack.getProgram()!.name,
+                pda: pdaNode(node.pda.name, seeds),
+              });
+            } else if (seeds) {
+              newPda = pdaLinkNode(newName ?? node.name);
+              pdasToUpsert.push({
+                program: stack.getProgram()!.name,
+                pda: pdaNode(newName ?? node.name, seeds),
+              });
             }
 
-            const newName = mainCase(updates.name ?? node.name);
             return accountNode({
               ...node,
-              ...updates,
+              ...assignableUpdates,
               data: accountDataNode({
                 ...node.data,
                 name: `${newName}AccountData`,
                 struct: renameStructNode(node.data.struct, updates.data ?? {}),
               }),
+              pda: newPda,
             });
           },
-        };
+        },
+      ];
+
+      if (newName) {
+        transformers.push(
+          {
+            select: `[accountLinkNode]${name}`,
+            transform: (node) => {
+              assertIsNode(node, 'accountLinkNode');
+              if (node.importFrom) return node;
+              return accountLinkNode(newName);
+            },
+          },
+          {
+            select: `[pdaNode]${name}`,
+            transform: (node) => {
+              assertIsNode(node, 'pdaNode');
+              return pdaNode(newName, node.seeds);
+            },
+          },
+          {
+            select: `[pdaLinkNode]${name}`,
+            transform: (node) => {
+              assertIsNode(node, 'pdaLinkNode');
+              if (node.importFrom) return node;
+              return pdaLinkNode(newName);
+            },
+          }
+        );
       }
-    )
+
+      if (pdasToUpsert.length > 0) {
+        transformers.push({
+          select: `[programNode]`,
+          transform: (node) => {
+            assertIsNode(node, 'programNode');
+            const pdasToUpsertForProgram = pdasToUpsert
+              .filter((p) => p.program === node.name)
+              .map((p) => p.pda);
+            if (pdasToUpsertForProgram.length === 0) return node;
+            const existingPdaNames = new Set(node.pdas.map((pda) => pda.name));
+            const pdasToCreate = pdasToUpsertForProgram.filter(
+              (p) => !existingPdaNames.has(p.name)
+            );
+            const pdasToUpdate = new Map(
+              pdasToUpsertForProgram
+                .filter((p) => existingPdaNames.has(p.name))
+                .map((p) => [p.name, p])
+            );
+            const newPdas = [
+              ...node.pdas.map((p) => pdasToUpdate.get(p.name) ?? p),
+              ...pdasToCreate,
+            ];
+            return programNode({ ...node, pdas: newPdas });
+          },
+        });
+      }
+
+      return transformers;
+    })
   );
 }
