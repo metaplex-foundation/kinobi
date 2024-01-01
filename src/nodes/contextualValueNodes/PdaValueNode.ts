db4ff8f2f0bd3a193c721d39cee026cf6c2cc39d
Loris Leiva <loris.leiva@gmail.com>
2024-01-01 20:27:56 +0000

    Update PdaValueNode.ts

--- src/nodes/contextualValueNodes/PdaValueNode.ts
+++ src/nodes/contextualValueNodes/PdaValueNode.ts
@@ -1,8 +1,10 @@
 import { MainCaseString } from '../../shared';
+import { isNode } from '../Node';
+import { PdaNode } from '../PdaNode';
 import { PdaLinkNode } from '../linkNodes';
 import { ValueNode } from '../valueNodes';
-import { AccountValueNode } from './AccountValueNode';
-import { ArgumentValueNode } from './ArgumentValueNode';
+import { AccountValueNode, accountValueNode } from './AccountValueNode';
+import { ArgumentValueNode, argumentValueNode } from './ArgumentValueNode';
 
 export type PdaValueNode = {
   readonly kind: 'pdaValueNode';
@@ -18,4 +20,18 @@ export function pdaValueNode(
   seeds: PdaValueNode['seeds'] = {}
 ): PdaValueNode {
   return { kind: 'pdaValueNode', pda, seeds };
+}
+
+export function getDefaultSeedValuesFromPda(
+  node: PdaNode
+): PdaValueNode['seeds'] {
+  return node.seeds.reduce((acc, seed) => {
+    if (!isNode(seed, 'variablePdaSeedNode')) return acc;
+    if (isNode(seed.type, 'publicKeyTypeNode')) {
+      acc[seed.name] = accountValueNode(seed.name);
+    } else {
+      acc[seed.name] = argumentValueNode(seed.name);
+    }
+    return acc;
+  }, {} as PdaValueNode['seeds']);
 }
