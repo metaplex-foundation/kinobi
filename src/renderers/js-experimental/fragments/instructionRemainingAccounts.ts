4c1b3fa7994fec0041e72b93449919e650a74bb8
Loris Leiva <loris.leiva@gmail.com>
2024-01-03 20:53:22 +0000

    Refactor instructionRemainingAccounts fragment to match the byteDelta fragment

--- src/renderers/js-experimental/fragments/instructionRemainingAccounts.ts
+++ src/renderers/js-experimental/fragments/instructionRemainingAccounts.ts
@@ -1,15 +1,12 @@
+import { camelCase } from '../../../shared';
 import {
   InstructionNode,
   InstructionRemainingAccountsNode,
+  assertIsNode,
   isNode,
 } from '../../../nodes';
 import type { GlobalFragmentScope } from '../getRenderMapVisitor';
-import {
-  Fragment,
-  fragment,
-  fragmentFromTemplate,
-  mergeFragments,
-} from './common';
+import { Fragment, fragment, mergeFragments } from './common';
 
 export function getInstructionRemainingAccountsFragment(
   scope: Pick<GlobalFragmentScope, 'nameApi' | 'asyncResolvers'> & {
@@ -19,7 +16,7 @@ export function getInstructionRemainingAccountsFragment(
 ): Fragment {
   const { remainingAccounts } = scope.instructionNode;
   const fragments = (remainingAccounts ?? []).flatMap((r) =>
-    getSingleFragment(r, scope)
+    getRemainingAccountsFragment(r, scope)
   );
   if (fragments.length === 0) return fragment('');
   return mergeFragments(
@@ -27,40 +24,63 @@ export function getInstructionRemainingAccountsFragment(
     (r) =>
       `// Remaining accounts.\n` +
       `const remainingAccounts: IAccountMeta[] = [...${r.join(', ...')}]`
-  );
+  ).addImports('solanaInstructions', ['IAccountMeta']);
 }
 
-function getSingleFragment(
+function getRemainingAccountsFragment(
   remainingAccounts: InstructionRemainingAccountsNode,
   scope: Pick<GlobalFragmentScope, 'nameApi' | 'asyncResolvers'> & {
     instructionNode: InstructionNode;
     useAsync: boolean;
   }
 ): Fragment[] {
-  const isAsync =
-    isNode(remainingAccounts.value, 'resolverValueNode') &&
-    scope.asyncResolvers.includes(remainingAccounts.value.name);
-  if (!scope.useAsync && isAsync) return [];
-
-  const remainingAccountsFragment = fragmentFromTemplate(
-    'instructionRemainingAccounts.njk',
-    {
-      remainingAccounts,
-      awaitKeyword: scope.useAsync && isAsync ? 'await ' : '',
-      nameApi: scope.nameApi,
+  const remainingAccountsFragment = ((): Fragment | null => {
+    if (isNode(remainingAccounts.value, 'argumentValueNode')) {
+      return getArgumentValueNodeFragment(remainingAccounts);
     }
-  ).addImports('solanaInstructions', ['IAccountMeta']);
+    if (isNode(remainingAccounts.value, 'resolverValueNode')) {
+      return getResolverValueNodeFragment(remainingAccounts, scope);
+    }
+    return null;
+  })();
 
-  if (isNode(remainingAccounts.value, 'argumentValueNode')) {
-    remainingAccountsFragment.addImports('solanaInstructions', ['AccountRole']);
-  } else {
-    const functionName = scope.nameApi.resolverFunction(
-      remainingAccounts.value.name
-    );
-    remainingAccountsFragment
-      .addImports(remainingAccounts.value.importFrom ?? 'hooked', functionName)
-      .addFeatures(['instruction:resolverScopeVariable']);
-  }
-
+  if (remainingAccountsFragment === null) return [];
   return [remainingAccountsFragment];
+}
+
+function getArgumentValueNodeFragment(
+  remainingAccounts: InstructionRemainingAccountsNode
+): Fragment {
+  assertIsNode(remainingAccounts.value, 'argumentValueNode');
+  const argumentName = camelCase(remainingAccounts.value.name);
+  const isWritable = remainingAccounts.isWritable ?? false;
+  const nonSignerRole = isWritable
+    ? 'AccountRole.WRITABLE'
+    : 'AccountRole.READONLY';
+  const signerRole = isWritable
+    ? 'AccountRole.WRITABLE_SIGNER'
+    : 'AccountRole.READONLY_SIGNER';
+  const role = remainingAccounts.isSigner === true ? signerRole : nonSignerRole;
+  return fragment(
+    `args.${argumentName}.map((address) => ({ address, role: ${role} }))`
+  ).addImports('solanaInstructions', ['AccountRole']);
+}
+
+function getResolverValueNodeFragment(
+  remainingAccounts: InstructionRemainingAccountsNode,
+  scope: Pick<GlobalFragmentScope, 'nameApi' | 'asyncResolvers'> & {
+    useAsync: boolean;
+  }
+): Fragment | null {
+  assertIsNode(remainingAccounts.value, 'resolverValueNode');
+  const isAsync = scope.asyncResolvers.includes(remainingAccounts.value.name);
+  if (!scope.useAsync && isAsync) return null;
+
+  const awaitKeyword = scope.useAsync && isAsync ? 'await ' : '';
+  const functionName = scope.nameApi.resolverFunction(
+    remainingAccounts.value.name
+  );
+  return fragment(`${awaitKeyword}${functionName}(resolverScope)`)
+    .addImports(remainingAccounts.value.importFrom ?? 'hooked', functionName)
+    .addFeatures(['instruction:resolverScopeVariable']);
 }
