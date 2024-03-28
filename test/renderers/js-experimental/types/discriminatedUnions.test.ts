b355097b23d3827c47e60ba2a4d60b273a799fa2
Loris Leiva <loris.leiva@gmail.com>
2024-03-28 10:58:10 +0000

    Add more tests

--- test/renderers/js-experimental/types/discriminatedUnions.test.ts
+++ test/renderers/js-experimental/types/discriminatedUnions.test.ts
@@ -31,14 +31,62 @@ const eventTypeNode = definedTypeNode({
   ]),
 });
 
+test('it exports discriminated union types', (t) => {
+  // When we render a discriminated union.
+  const renderMap = visit(eventTypeNode, getRenderMapVisitor());
+
+  // Then we expect the following types to be exported.
+  renderMapContains(t, renderMap, 'types/event.ts', [
+    'export type Event =',
+    "| { __kind: 'Quit' }",
+    "| { __kind: 'Write'; fields: readonly [string] }",
+    "| { __kind: 'Move'; x: number; y: number }",
+  ]);
+});
+
 test('it exports discriminated union codecs', (t) => {
   // When we render a discriminated union.
   const renderMap = visit(eventTypeNode, getRenderMapVisitor());
 
-  // Then we expect the following codec helpers to be exported.
+  // Then we expect the following codec functions to be exported.
   renderMapContains(t, renderMap, 'types/event.ts', [
     'export function getEventEncoder(): Encoder< EventArgs >',
     'export function getEventDecoder(): Decoder< Event >',
     'export function getEventCodec(): Codec< EventArgs, Event >',
+  ]);
+});
+
+test('it exports discriminated union helpers', (t) => {
+  // When we render a discriminated union.
+  const renderMap = visit(eventTypeNode, getRenderMapVisitor());
+
+  // Then we expect the following helpers to be exported.
+  renderMapContains(t, renderMap, 'types/event.ts', [
+    "export function event( kind: 'Quit' ): GetDiscriminatedUnionVariant< EventArgs, '__kind', 'Quit' >;",
+    "export function event( kind: 'Write', data: GetDiscriminatedUnionVariantContent< EventArgs, '__kind', 'Write' >[ 'fields' ] ): GetDiscriminatedUnionVariant< EventArgs, '__kind', 'Write' >;",
+    "export function event( kind: 'Move', data: GetDiscriminatedUnionVariantContent< EventArgs, '__kind', 'Move' > ): GetDiscriminatedUnionVariant< EventArgs, '__kind', 'Move' >;",
+    "export function isEvent< K extends Event['__kind'] >( kind: K, value: Event ): value is Event & { __kind: K }",
+  ]);
+});
+
+test('it exports discriminated union with custom discriminator properties', (t) => {
+  // When we render a discriminated union with a custom discriminator property.
+  const renderMap = visit(
+    eventTypeNode,
+    getRenderMapVisitor({
+      nameTransformers: { discriminatedUnionDiscriminator: () => `type` },
+    })
+  );
+
+  // Then we expect the discriminator property to be used instead of __kind.
+  renderMapContains(t, renderMap, 'types/event.ts', [
+    "{ discriminator: 'type' }",
+    "| { type: 'Quit' }",
+    "| { type: 'Write'; fields: readonly [string] }",
+    "| { type: 'Move'; x: number; y: number }",
+    "export function event( kind: 'Quit' ): GetDiscriminatedUnionVariant< EventArgs, 'type', 'Quit' >;",
+    "export function event( kind: 'Write', data: GetDiscriminatedUnionVariantContent< EventArgs, 'type', 'Write' >[ 'fields' ] ): GetDiscriminatedUnionVariant< EventArgs, 'type', 'Write' >;",
+    "export function event( kind: 'Move', data: GetDiscriminatedUnionVariantContent< EventArgs, 'type', 'Move' > ): GetDiscriminatedUnionVariant< EventArgs, 'type', 'Move' >;",
+    "export function isEvent< K extends Event['type'] >( kind: K, value: Event ): value is Event & { type: K }",
   ]);
 });
