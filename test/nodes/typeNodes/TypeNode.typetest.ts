3fa2bc5aa7681771c1a4fbbec9da7bbc32d2bb9c
Loris Leiva <loris.leiva@gmail.com>
2024-04-06 16:51:31 +0100

    wip

--- test/nodes/typeNodes/TypeNode.typetest.ts
+++ test/nodes/typeNodes/TypeNode.typetest.ts
@@ -2,11 +2,14 @@ import {
   fixedSizeTypeNode,
   NumberTypeNode,
   ResolveNestedTypeNode,
+  StringTypeNode,
   stringTypeNode,
 } from '../../../src';
 
 {
-  // [ResolveNestedTypeNode]: TODO
+  // [ResolveNestedTypeNode]: it constraints the nested type of a node.
   const node = fixedSizeTypeNode(stringTypeNode(), 32);
+  node satisfies ResolveNestedTypeNode<StringTypeNode>;
+  // @ts-expect-error The nested type is not a number.
   node satisfies ResolveNestedTypeNode<NumberTypeNode>;
 }
