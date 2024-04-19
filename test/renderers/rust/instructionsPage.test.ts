49f7a0436464840c9d8c6852b23371b48e80943e
febo <febo@tensor.so>
2024-04-19 11:39:44 +0100

    Add test

---
+++ test/renderers/rust/instructionsPage.test.ts
@@ -0,0 +1,21 @@
+import test from 'ava';
+import { instructionNode, programNode, visit } from '../../../src';
+import { getRenderMapVisitor } from '../../../src/renderers/rust/getRenderMapVisitor';
+import { codeContains } from './_setup';
+
+test('it renders a public instruction data struct', (t) => {
+  // Given the following program with 1 instruction.
+  const node = programNode({
+    name: 'splToken',
+    publicKey: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+    instructions: [instructionNode({ name: 'mintTokens' })],
+  });
+
+  // When we render it.
+  const renderMap = visit(node, getRenderMapVisitor());
+
+  // Then we expect the following pub struct.
+  codeContains(t, renderMap.get('instructions/mint_tokens.rs'), [
+    `pub struct MintTokensInstructionData`,
+  ]);
+});
