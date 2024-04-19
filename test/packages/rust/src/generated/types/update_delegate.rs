fda795d197ebf8ffba059862f85283ef640b149f
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-04-19 02:22:32 -0700

    Regenerate test clients

--- test/packages/rust/src/generated/types/update_delegate.rs
+++ test/packages/rust/src/generated/types/update_delegate.rs
@@ -9,21 +9,12 @@ use borsh::BorshDeserialize;
 use borsh::BorshSerialize;
 use solana_program::pubkey::Pubkey;
 
-/// This is a union of all the possible payload types.
 #[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum PayloadType {
+pub struct UpdateDelegate {
     #[cfg_attr(
         feature = "serde",
-        serde(with = "serde_with::As::<serde_with::DisplayFromStr>")
+        serde(with = "serde_with::As::<Vec<serde_with::DisplayFromStr>>")
     )]
-    Pubkey(Pubkey),
-    Seeds {
-        seeds: Vec<Vec<u8>>,
-    },
-    MerkleProof {
-        leaf: [u8; 32],
-        proof: Vec<[u8; 32]>,
-    },
-    Number(u64),
+    pub additional_delegates: Vec<Pubkey>,
 }
